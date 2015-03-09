853718582367fdf465de1bab44e14b0259cbdc18
root <root@ip-172-31-57-32.ec2.internal>
2015-03-09 00:51:59 +0000

    Finished form.php

--- Part1c/form.php
+++ Part1c/form.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-
-</head>
-<body>
-
-</body>
+    <head>
+	
+   
+	
+    </head>
+    <body>
+	<fieldset>
+	    <legend>Passenger Info</legend>
+	    <form action="form_validation.php">
+		<label class="heading" for="firstname">First Name: </label>
+		<input type="text" name="firstname" /> <br />
+		<label class="heading" for="lastname"> Last Name: </label>
+		<input type="text" name="lastname" /> <br />
+		<label class="heading" for="ssn"> SSN: </label>
+		<input type="text" name="ssn" /> <br />
+	    </form>
+	</fieldset>
+    </body>
 </html>
