e5cba86050922dbfa474a1d1d2a97ae8e1f7bec0
Joshua Nance <jnance@pugetsound.edu>
2015-03-11 16:10:22 -0700

    update form

---
+++ Part1c/update.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+  
+  <form action="up.php" method="post">
+    
+    <input type="text" name="ssn" value=<?php echo $_GET['ssn'] ?>/>
+    <input type="submit"/>
+  </form>
+
+</body>
+</html>
