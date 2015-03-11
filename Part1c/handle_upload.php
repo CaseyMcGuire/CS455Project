5c62796ec458bdba896baf33590a629e969a6b80
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 18:44:51 -0700

    formstuff

--- Part1c/handle_upload.php
+++ Part1c/handle_upload.php
@@ -1,8 +1,8 @@
 <?php
 $upload_directory = "uploads/";
-$file_to_upload = $upload_directory . basename($_POST["filename"]);
+$file_to_upload = $upload_directory . basename($_FILES["filename"]["name"]);
 if (move_uploaded_file($_FILES["filename"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "The file ". basename( $_FILES["fileToUpload"]["tmp_name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
