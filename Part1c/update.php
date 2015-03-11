01e50c16f6445f878f3a2153cafd309c858dc261
Joshua Nance <jnance@pugetsound.edu>
2015-03-11 16:32:03 -0700

    adf

--- Part1c/update.php
+++ Part1c/update.php
@@ -4,6 +4,7 @@
   <?php
   
   echo "<form action=\"update2.php\" method=\"post\">";
+  echo "<label class=\"heading\" for=\"lastname\"> SSN: </label>";
   echo "<input type=\"text\" name=\"ssn\" value=\"".$_GET['ssn']."\"\>";
   echo "<input type=\"text\" name=\"l_name\" value=\"".$_GET['l_name']."\"\>";
   echo "<input type=\"text\" name=\"seat\" value=\"".$_GET['seat']."\"\>";
