7d44703e6ceb5abce35957cea1d12fa8f7aa01d1
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 18:49:36 -0700

    fix forms

--- Part1c/handle_upload.php
+++ Part1c/handle_upload.php
@@ -2,7 +2,7 @@
 $upload_directory = "uploads/";
 $file_to_upload = $upload_directory . basename($_FILES["filename"]["name"]);
 
-if (move_uploaded_files($_FILES["filename"]["tmp_name"], $file_to_upload)) {
+if (move_uploaded_file($_FILES["filename"]["tmp_name"], $file_to_upload)) {
   echo "The file ".basename($_FILES["filename"]["tmp_name"])." has been uploaded.";
 } else {
   echo "Please try again.";
