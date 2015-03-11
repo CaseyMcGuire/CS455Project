c5a16c4c05e59a5fde48a84af3853e67595dc5b4
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 19:03:26 -0700

    filelname

--- Part1c/handle_upload.php
+++ Part1c/handle_upload.php
@@ -3,7 +3,7 @@ $upload_directory = "uploads/";
 $file_to_upload = $upload_directory . basename($_FILES["filename"]["name"]);
 
 if (move_uploaded_file($_FILES["filename"]["tmp_name"], $file_to_upload)) {
-  echo "The file ".basename($_FILES["filename"]["tmp_name"])." has been uploaded.";
+  echo "The file ".basename($_FILES["filename"]["name"])." has been uploaded.";
 } else {
   echo "Please try again.";
   print_r($_FILES);
