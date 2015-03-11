1f06a19a739447fdd2b1ff902cc468ac0dacdbf3
Joshua Nance <jnance@pugetsound.edu>
2015-03-11 15:47:21 -0700

    update link

--- Part1c/myFirstDB.php
+++ Part1c/myFirstDB.php
@@ -27,11 +27,10 @@ try
                 echo "<td>".$tuple['dep_loc']." ".$tuple['dep_time']."</td>";
                 echo "<td>".$tuple['arr_loc']." ".$tuple['arr_time']."</td>";
                 echo "<td><a href=\"delete.php?ssn=".$tuple['ssn']."&flight_no=".$tuple['flight_no']."\">Delete</a></td>";
-                echo "<td>Update</td>";
                 echo "</tr>";
         }
-
         echo "</table>";
+        echo "<a href=\"update.php\"</a>";
 
         $db = NULL;
 
