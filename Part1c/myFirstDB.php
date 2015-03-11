c0a67eb42fd4772a71a5c8118c5471b344b485bf
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 18:12:20 -0700

    skeleton for form for number 3

---
+++ Part1c/myFirstDB.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+try
+{
+        $db = new PDO('sqlite:database/airport.sqlite3');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        //These need to be commented out. Otherwise, we get a constraint error.
+        //      $db->exec("insert into passengers values ('David', NULL, 'Chiu', '888-88-8888');");
+        //      $db->exec("insert into passengers values ('Brad', NULL, 'Richards', '999-99-9999');");
+
+        //      $db->exec("insert into onboard values ('888-88-8888', 4, '32B');");
+        //      $db->exec("insert into onboard values ('999-99-9999', 4, '32C');");
+
+        echo '<table border="1">';
+        echo '<tr><td>SSN</td><td>Name</td><td>Seat</td><td>Departure</td><td>Arrival</td></tr>';
+
+        $result = $db->query('SELECT * FROM passengers NATURAL JOIN onboard NATURAL JOIN flight');
+
+        foreach($result as $tuple)
+        {
+                echo "<tr><td>".$tuple['ssn']."</td>";
+                echo "<td>".$tuple['f_name']." ".$tuple['l_name']."</td>";
+                echo "<td>".$tuple['seat']."</td>";
+                echo "<td>".$tuple['dep_loc']." ".$tuple['dep_time']."</td>";
+                echo "<td>".$tuple['arr_loc']." ".$tuple['arr_time']."</td>";
+                echo "</tr>";
+        }
+
+        echo "</table>";
+
+        $db = NULL;
+
+}
+
+catch(PDOException $e)
+{
+        print 'Exception : '.$e->getMessage();
+}
+?>
+</body>
+</html>
---
+++ Part1c/update.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Upload a File</title>
+</head>
+
+<body>
+  
+  <form action="handle_upload.php" method="post">
+    <label for="uploaded">Upload file:</label>
+    <input type="file" name="uploaded"/>
+  </form>
+  
+  <?php
+  if (!isset($_GET["uploaded"])) {
+    echo "Please select a file to upload."
+  }
+  ?>
+  
+</body>
+
+</html>
