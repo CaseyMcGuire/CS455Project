d4a3516aed103723c29f70942849d7a4bcf76788
Joshua Nance <jnance@pugetsound.edu>
2015-03-11 16:17:38 -0700

    asdfa

--- Part1c/update.php
+++ Part1c/update.php
@@ -4,7 +4,7 @@
   <?php
   
   echo "<form action=\"update2.php\" method=\"post\">";
-  echo "<input type=\"text\" name=\"ssn\" value=\"".$_GET['ssn']"\"\>";
+  echo "<input type=\"text\" name=\"ssn\" value=\"".$_GET['ssn']."\"\>";
   echo "<input type=\"submit\"\\>";
   ?>
 </body>
