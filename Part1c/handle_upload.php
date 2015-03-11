afb136b13c6ea51e3dabfd7f020275bd055dfcfd
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 19:17:43 -0700

    format file submission response

--- Part1c/handle_upload.php
+++ Part1c/handle_upload.php
@@ -3,7 +3,12 @@ $upload_directory = "uploads/";
 $file_to_upload = $upload_directory . basename($_FILES["filename"]["name"]);
 
 if (move_uploaded_file($_FILES["filename"]["tmp_name"], $file_to_upload)) {
-  echo "The file ".basename($_FILES["filename"]["name"])." has been uploaded.";
+  
+  $file = basename($_FILES["filename"]["name"]);
+  echo "$file has been uploaded."
+    
+  echo "$file is " . filesize("uploads/".$file);
+  
 } else {
   echo "Please try again.";
   print_r($_FILES);
