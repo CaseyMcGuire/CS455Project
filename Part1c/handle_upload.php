f0aab345ef2f1e571b552e5e2ee4859e5c5972c1
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 19:38:08 -0700

    ad

--- Part1c/handle_upload.php
+++ Part1c/handle_upload.php
@@ -2,13 +2,22 @@
 $upload_directory = "uploads/";
 $file_to_upload = $upload_directory . basename($_FILES["filename"]["name"]);
 
+
+
+function human_filesize($bytes, $decimals = 2) {
+    $size = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
+    $factor = floor((strlen($bytes) - 1) / 3);
+    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+}
+
+
 if (move_uploaded_file($_FILES["filename"]["tmp_name"], $file_to_upload)) {
   
   $file = basename($_FILES["filename"]["name"]);
   
-  echo "$file has been uploaded.";
+  echo "$file has been uploaded.\n";
   
-  $file_size = filesize("uploads/$file");
+  $file_size = human_filesize("uploads/$file", 2);
   echo "$file is $file_size in size.";
   
 } else {
