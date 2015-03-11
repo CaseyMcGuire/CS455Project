296420c1c33a2d265e1977102268977303e9f18d
Joshua Nance <jnance@pugetsound.edu>
2015-03-11 16:44:56 -0700

    form stuff

--- Part1c/update.php
+++ Part1c/update.php
@@ -4,12 +4,22 @@
   <?php
   
   echo "<form action=\"update2.php\" method=\"post\">";
-  echo "<label class=\"heading\" for=\"lastname\"> SSN: </label>";
+  
+  echo "<label class=\"heading\" for=\"ssn\"> SSN: </label>";
   echo "<input type=\"text\" name=\"ssn\" value=\"".$_GET['ssn']."\"\>";
+  
+  echo "<label class=\"heading\" for=\"l_name\"> Last Name: </label>";  
   echo "<input type=\"text\" name=\"l_name\" value=\"".$_GET['l_name']."\"\>";
+
+  echo "<label class=\"heading\" for=\"seat\"> Seat: </label>";
   echo "<input type=\"text\" name=\"seat\" value=\"".$_GET['seat']."\"\>";
+  
+  echo "<label class=\"heading\" for=\"dep_loc\"> Departure Location: </label>";
   echo "<input type=\"text\" name=\"dep_loc\" value=\"".$_GET['dep_loc']."\"\>";
+  
+  echo "<label class=\"heading\" for=\"arr_loc\"> Arrival Location: </label>";
   echo "<input type=\"text\" name=\"arr_loc\" value=\"".$_GET['arr_loc']."\"\>";
+  
   echo "<input type=\"submit\"\\>";
   ?>
 </body>
