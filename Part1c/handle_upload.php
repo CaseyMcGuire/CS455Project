cba5ef63f0950b5e4f4858b1bd5be9e0087a78eb
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 18:41:00 -0700

    fixing form handler

--- Part1c/handle_upload.php
+++ Part1c/handle_upload.php
@@ -1,6 +1,6 @@
 <?php
 $upload_directory = "uploads/";
-$file_to_upload = $upload_directory . basename($_FILES["filename"]["name"]);
+$file_to_upload = $upload_directory . basename($_POST["filename"]);
 if (move_uploaded_file($_FILES["filename"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
