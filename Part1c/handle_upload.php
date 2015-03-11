d9d89af54b9d9b8a4ba10e67be732424187b00d5
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 18:38:17 -0700

    trying to add file upload script

---
+++ Part1c/handle_upload.php
@@ -0,0 +1,9 @@
+<?php
+$upload_directory = "uploads/";
+$file_to_upload = $upload_directory . basename($_FILES["filename"]["name"]);
+if (move_uploaded_file($_FILES["filename"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+?>
