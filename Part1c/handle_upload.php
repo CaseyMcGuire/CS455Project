6af26a43aa093f84d846d2b85214024193c29670
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 19:20:18 -0700

    formattig response

--- Part1c/handle_upload.php
+++ Part1c/handle_upload.php
@@ -5,8 +5,11 @@ $file_to_upload = $upload_directory . basename($_FILES["filename"]["name"]);
 if (move_uploaded_file($_FILES["filename"]["tmp_name"], $file_to_upload)) {
   
   $file = basename($_FILES["filename"]["name"]);
+  
   echo "$file has been uploaded.";
-  echo "$file is " . filesize("uploads/".$file);
+  
+  $file_size = filesize("uploads/$file");
+  echo "$file is $filesize large";
   
 } else {
   echo "Please try again.";
