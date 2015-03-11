8cb5f9e08646aab61c3f6ee8f1f3bba6577fe848
Joshua Nance <jnance@pugetsound.edu>
2015-03-10 19:19:10 -0700

    fixing response

--- Part1c/handle_upload.php
+++ Part1c/handle_upload.php
@@ -5,8 +5,7 @@ $file_to_upload = $upload_directory . basename($_FILES["filename"]["name"]);
 if (move_uploaded_file($_FILES["filename"]["tmp_name"], $file_to_upload)) {
   
   $file = basename($_FILES["filename"]["name"]);
-  echo "$file has been uploaded."
-    
+  echo "$file has been uploaded.";
   echo "$file is " . filesize("uploads/".$file);
   
 } else {
