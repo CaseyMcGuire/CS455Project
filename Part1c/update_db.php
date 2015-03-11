db3b7de0c64687626ba8938c6e7c9bf55ea74329
Joshua Nance <jnance@pugetsound.edu>
2015-03-11 16:53:51 -0700

    change to for loop

--- Part1c/update_db.php
+++ Part1c/update_db.php
@@ -3,7 +3,11 @@ try {
   
   $db = new PDO('sqlite:../../database/airport.sqlite3');
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+  
+  
   echo "hi";
+  
 }
 catch(PDOException $e) {
   print $e->getMessage();
--- Part1c/update_form.php
+++ Part1c/update_form.php
@@ -5,30 +5,37 @@
   
   echo "<form action=\"update_db.php\" method=\"post\">";
   
-  echo "<label class=\"heading\" for=\"ssn\"> SSN: </label>";
-  echo "<input type=\"text\" name=\"ssn\" value=\"".$_GET['ssn']."\"\>";
-  echo "<br />";
-  echo "<br />";
+  // echo "<label class=\"heading\" for=\"ssn\"> SSN: </label>";
+  // echo "<input type=\"text\" name=\"ssn\" value=\"".$_GET['ssn']."\"\>";
+  // echo "<br />";
+  // echo "<br />";
+  //
+  // echo "<label class=\"heading\" for=\"l_name\"> Last Name: </label>";
+  // echo "<input type=\"text\" name=\"l_name\" value=\"".$_GET['l_name']."\"\>";
+  // echo "<br />";
+  // echo "<br />";
+  //
+  // echo "<label class=\"heading\" for=\"seat\"> Seat: </label>";
+  // echo "<input type=\"text\" name=\"seat\" value=\"".$_GET['seat']."\"\>";
+  // echo "<br />";
+  // echo "<br />";
+  //
+  // echo "<label class=\"heading\" for=\"dep_loc\"> Departure Location: </label>";
+  // echo "<input type=\"text\" name=\"dep_loc\" value=\"".$_GET['dep_loc']."\"\>";
+  // echo "<br />";
+  // echo "<br />";
+  //
+  // echo "<label class=\"heading\" for=\"arr_loc\"> Arrival Location: </label>";
+  // echo "<input type=\"text\" name=\"arr_loc\" value=\"".$_GET['arr_loc']."\"\>";
+  // echo "<br />";
+  // echo "<br />";
   
-  echo "<label class=\"heading\" for=\"l_name\"> Last Name: </label>";  
-  echo "<input type=\"text\" name=\"l_name\" value=\"".$_GET['l_name']."\"\>";
-  echo "<br />";
-  echo "<br />"; 
-  
-  echo "<label class=\"heading\" for=\"seat\"> Seat: </label>";
-  echo "<input type=\"text\" name=\"seat\" value=\"".$_GET['seat']."\"\>";
-  echo "<br />";
-  echo "<br />";
-      
-  echo "<label class=\"heading\" for=\"dep_loc\"> Departure Location: </label>";
-  echo "<input type=\"text\" name=\"dep_loc\" value=\"".$_GET['dep_loc']."\"\>";
-  echo "<br />";
-  echo "<br />";
-      
-  echo "<label class=\"heading\" for=\"arr_loc\"> Arrival Location: </label>";
-  echo "<input type=\"text\" name=\"arr_loc\" value=\"".$_GET['arr_loc']."\"\>";
-  echo "<br />";
-  echo "<br />";
+  foreach($_GET as $query_string => $variable) {
+    echo "<label class=\"heading\" for=\"$query_string\"> Arrival Location: </label>";
+    echo "<input type=\"text\" name=\"$query_string\" value=\"".$variable."\"\>";
+    echo "<br />";
+    echo "<br />";
+  }
       
   echo "<input type=\"submit\"\\>";
   ?>
