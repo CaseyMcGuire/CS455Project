c0cd7f0e53c62fd3e94d8e66cd3b30aeadd452cd
root <root@ip-172-31-57-32.ec2.internal>
2015-03-12 23:16:54 +0000

    clean up

--- Part1c/index.php
+++ Part1c/index.php
@@ -7,13 +7,6 @@ try
         $db = new PDO('sqlite:../../database/airport.sqlite3');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        //These need to be commented out. Otherwise, we get a constraint error.
-        //      $db->exec("insert into passengers values ('David', NULL, 'Chiu', '888-88-8888');");
-        //      $db->exec("insert into passengers values ('Brad', NULL, 'Richards', '999-99-9999');");
-
-        //      $db->exec("insert into onboard values ('888-88-8888', 4, '32B');");
-        //      $db->exec("insert into onboard values ('999-99-9999', 4, '32C');");
-
         echo '<table border="1">';
         echo '<tr><td>SSN</td><td>Flight No.</td><td>Name</td><td>Seat</td><td>Departure</td><td>Arrival</td></tr>';
 
--- Part1c/update_db.php
+++ Part1c/update_db.php
@@ -18,15 +18,16 @@ try {
     //update the passengers name
   $db->exec("update passengers set f_name='$new_f_name', l_name='$new_l_name' where ssn='$ssn'");
 
-    //update the flight's departure and arrival locations
-//  $db->exec("update flight set dep_loc='$new_dep_loc', arr_loc='$new_arr_loc' where flight_no='$old_flight_no'");
+    //update the flight's departure, arrival locations, and flight number
     $db->exec("update flight set dep_loc='$new_dep_loc', arr_loc='$new_arr_loc', flight_no=$new_flight_no where flight_no=$old_flight_no");
-    $db->exec("update onboard set flight_no=$new_flight_no where flight_no=$old_flight_no");
-    //$db->exec("update onboard set flight_no=$new_flight_no, seat='$new_seat' where flight_no=$old_flight_no and ssn='$ssn'");
-
-    $db->exec("update onboard set seat='$new_seat' where ssn='$ssn' and flight_no=$new_flight_no");
-     $db = NULL;
     
+    //update flight number in onboard table (this shouldn't be necessary if the table is cascading
+    //properly but it doesn't seem to be).
+    $db->exec("update onboard set flight_no=$new_flight_no where flight_no=$old_flight_no");
+
+    //update the passenger's seat 
+    $db->exec("update onboard set seat='$new_seat' where ssn='$ssn' and flight_no=$new_flight_no");
+    $db = NULL;
     
     header("Location: index.php");
     exit;
