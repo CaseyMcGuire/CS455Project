4b6fb882db85ff119dc562f5c5d4cd13d5197eef
Joshua Nance <jnance@pugetsound.edu>
2015-03-11 18:06:29 -0700

    finishing up update form

--- Part1c/update_db.php
+++ Part1c/update_db.php
@@ -3,17 +3,32 @@ try {
   
   $db = new PDO('sqlite:../../database/airport.sqlite3');
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
+
+  $old_ssn = $_POST['old_ssn'];  
   $new_ssn = $_POST['ssn'];
-  $old_ssn = $_POST['old_ssn'];
-  
-  print "$old_ssn";
-  print "$new_ssn";
-  
+  $db->exec("update passengers set ssn=$new_ssn where ssn=$old_ssn");
+
+  $old_l_name = $_POST['old_l_name'];
   $new_l_name = $_POST['l_name'];
+  $db->exec("update passengers set l_name=$new_l_name where l_name=$old_l_name");
+  
+  $old_seat = $_POST['old_seat'];
   $new_seat = $_POST['seat'];
-  $new_dep_loc = $_POST['dep_loc'];    
+  $db->exec("update onboard set seat=$new_seat where seat=$old_seat");
+  
+  $old_dep_loc = $_POST['old_dep_loc'];
+  $new_dep_loc = $_POST['dep_loc'];
+  $db->exec("update flight set dep_loc=$new_dep_loc where dep_loc=$old_dep_loc");
+  
+  $old_arr_loc = $_POST['old_arr_loc'];
   $new_arr_loc = $_POST['arr_loc'];
+  $db->exec("update flight set arr_loc=$new_arr_loc where arr_loc=$old_arr_loc");
+  
+  $db = NULL;
+  
+  header("Location: myFirstDB.php");
+  
+  
 }
 catch(PDOException $e) {
   print $e->getMessage();
