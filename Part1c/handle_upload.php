eba91b556bcae3839d9311ba40ca29a4f957f97a
Joshua Nance <jnance@pugetsound.edu>
2015-03-13 15:25:00 -0700

    add print statement to test

--- Part1c/handle_upload.php
+++ Part1c/handle_upload.php
@@ -14,6 +14,7 @@ if (move_uploaded_file($_FILES["filename"]["tmp_name"], $file_to_upload)) {
   
   if ($source = opendir($upload_directory)) {
     $to_download = readdir($source);
+    print $to_download;
     echo "<a href='download.php?file=".$to_download."'>Download</a>";
   }
   
