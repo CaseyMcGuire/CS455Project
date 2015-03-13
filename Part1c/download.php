63531d11837d78af2dab8d4dfa14445e9f902f89
Joshua Nance <jnance@pugetsound.edu>
2015-03-13 15:21:29 -0700

    adding headers

--- Part1c/download.php
+++ Part1c/download.php
@@ -6,6 +6,11 @@ if(!$file) {
   die("File not found.");
 }
 else {
+  header("Cache-Control: public");
+  header("Content-Description: File Transfer");
+  header("Content-Disposition: attachment; filename=$file");
+  header("Content-Type: application/zip");
+  header("Content-Transfer-Encoding: binary");
   readfile($file);
 }
 
