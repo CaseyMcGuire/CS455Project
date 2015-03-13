382e069c48859d479ea969d9530bdbc867e7a3b5
root <root@ip-172-31-57-32.ec2.internal>
2015-03-13 00:08:56 +0000

    form.php now requires properly formatted social security number

--- Part1c/form_validation.php
+++ Part1c/form_validation.php
@@ -1,10 +1,14 @@
 <?php
-print_r ($_POST);
+  //print_r ($_POST);
 echo "<br />";
 //First, make sure the user didn't enter empty fields
 $invalid_first_name = ($_POST["firstname"] == "");
 $invalid_last_name = ($_POST["lastname"] == "");
 $invalid_ssn = ($_POST["ssn"] == "");
+
+if(!preg_match("#\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b#", $_POST["ssn"])){
+  $invalid_ssn = true;
+}
 
 //if the user *did* enter any empty fields, send them back to the form page with a friendly reminder
 if($invalid_first_name || $invalid_last_name || $invalid_ssn){
@@ -20,7 +24,8 @@ try{
   $db->exec("insert into passengers values ('" . $_POST['firstname'] . "', NULL,'" . $_POST['lastname']. "','" . $_POST['ssn'] . "')");
 
   $db = NULL;
-  echo "SUCCESS";
+  echo "<marquee direction='right'><h1>SUCCESS</h1></marquee>";
+  echo "<a href='index.php'>Home</a>";
 }
 catch(PDOException $e){
   print 'Exception: ' . $e->getMessage();
