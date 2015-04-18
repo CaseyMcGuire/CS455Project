8328e0774efe75bcc60ad3dfe386b3a3372ce3fd
root <root@ip-172-31-57-32.ec2.internal>
2015-04-18 05:05:47 +0000

    Users can now signup

--- Part3/users/create.php
+++ Part3/users/create.php
@@ -12,10 +12,29 @@ try{
 
   var_dump($_POST);
 
-  
+  $email = $_POST['email'];
+  $password = crypt($_POST['password']);
 
-  $db = NULL;
+  $user_query = 'INSERT INTO User values (:email, :password, :screen_name)';
+  $user_query = $db->prepare($user_query);
 
+  $user_query->bindValue(':email', $email, SQLITE3_TEXT);
+  $user_query->bindValue(':password', $password, SQLITE3_TEXT);
+  $user_query->bindValue(':screen_name', $email, SQLITE3_TEXT);
+
+  $user_query->execute();
+
+  $result = $db->query('SELECT * FROM User');
+
+//    foreach($result as $tuple){
+//	echo $tuple['email'] . ' ' . $tuple['password'];
+//	echo '<br />';
+  //  }
+
+    header('Location:/');
+
+    $db = NULL;
+    
 }catch(PDOException $e){
   print 'Exception : ' . $e->getMessage();
 
