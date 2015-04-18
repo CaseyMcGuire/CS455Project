d8d9de378147b4fc14344b625b7982c8d6b2c364
root <root@ip-172-31-57-32.ec2.internal>
2015-04-18 01:44:15 +0000

    Worked on signup form

--- Part3/users/create.php
+++ Part3/users/create.php
@@ -1,3 +1,23 @@
 <?php
 
+if(strcmp($_POST['password'], $_POST['password_again']) != 0){
+  header('Location:new.php?password=1');
+}
+
+try{
+
+  $db = new PDO('sqlite:../database/blog.sqlite3');
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  var_dump($_POST);
+
+  
+
+  $db = NULL;
+
+}catch(PDOException $e){
+  print 'Exception : ' . $e->getMessage();
+
+ }
 ?>
--- Part3/users/new.php
+++ Part3/users/new.php
@@ -13,22 +13,26 @@ include("../util/header.php");
 
 		<div class="form-element">
 		<label for="email">Email</label>
-		<input id="email"  type="email" placeholder="Email">
+   <input id="email" name="email"  type="email" placeholder="Email">
 		</div>
 	
 		<div class="form-element">
 		<label for="password">Password</label>
-		<input id="password" type="password" placeholder="Password">
+		<input id="password" name="password" type="password" placeholder="Password">
 		</div>
 	
 		<div class="form-element">
 		<label for="password-again">Enter Your Password Again</label>
-		<input id="password-again" type="password" placeholder="Password">
+		<input id="password-again" name="password_again" type="password" placeholder="Password">
 		</div>
 	
 		<button type="submit" class="pure-button pure-button-primary">Create</button>
 		</fieldset>
 	    </form>
+	    <?php if(isset($_GET['password']) && $_GET['password']){ ?>
+		<h3>Password didn't match.</h3>
+	    <?php } ?>
+
     </div>
     </body>
 </html>
