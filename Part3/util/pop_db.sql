bc83111aed3d0423ba6516e401f513fa08a19512
Joshua Nance <jnance@pugetsound.edu>
2015-04-26 16:06:11 -0700

    adding db population file

---
+++ Part3/util/pop_db.sql
@@ -0,0 +1,52 @@
+insert into User values ('a@email', 'p4ssword_a', 'user_a');
+insert into User values ('b@email', 'p4ssword_b', 'user_b');
+insert into User values ('c@email', 'p4ssword_c', 'user_c');
+insert into User values ('d@email', 'p4ssword_d', 'user_d');
+insert into User values ('e@email', 'p4ssword_e', 'user_e');
+insert into User values ('f@email', 'p4ssword_f', 'user_f');
+insert into User values ('g@email', 'p4ssword_g', 'user_g');
+insert into User values ('h@email', 'p4ssword_h', 'user_h');
+  
+insert into Post values ('10', 'a@email', '10', '10', '2015-04-27', '22:14:01');
+insert into Post values ('11', 'a@email', '11', '11', '2015-04-27', '22:14:02');
+  
+insert into Post values ('12', 'b@email', '12', '12', '2015-04-27', '22:14:03');
+insert into Post values ('13', 'b@email', '13', '13', '2015-04-27', '22:14:04');
+  
+insert into Post values ('14', 'c@email', '14', '14', '2015-04-27', '22:14:05');
+insert into Post values ('15', 'c@email', '15', '15', '2015-04-27', '22:14:06');
+  
+insert into Post values ('16', 'd@email', '16', '16', '2015-04-27', '22:14:07');
+insert into Post values ('17', 'd@email', '17', '17', '2015-04-27', '22:14:08');
+
+insert into Post values ('18', 'e@email', '18', '18', '2015-04-27', '22:14:09');
+insert into Post values ('19', 'e@email', '19', '19', '2015-04-27', '22:14:10');
+  
+insert into Post values ('20', 'f@email', '20', '20', '2015-04-27', '22:14:11');
+insert into Post values ('21', 'f@email', '21', '21', '2015-04-27', '22:14:12');
+  
+insert into Post values ('22', 'g@email', '22', '22', '2015-04-27', '22:14:13');
+insert into Post values ('23', 'g@email', '23', '23', '2015-04-27', '22:14:14');
+  
+insert into Post values ('24', 'h@email', '24', '24', '2015-04-27', '22:14:15');
+insert into Post values ('25', 'h@email', '25', '25', '2015-04-27', '22:14:16');
+  
+insert into Faves values ('a@email', '12');
+insert into Faves values ('a@email', '13');
+insert into Faves values ('a@email', '14');
+
+insert into Faves values ('b@email', '10');
+insert into Faves values ('b@email', '14');
+insert into Faves values ('b@email', '15');
+  
+insert into Faves values ('c@email', '13');
+insert into Faves values ('c@email', '12');
+insert into Faves values ('c@email', '11');
+  
+insert into Faves values ('d@email', '20');
+insert into Faves values ('d@email', '22');
+insert into Faves values ('d@email', '24');
+  
+insert into Faves values ('e@email', '16');
+insert into Faves values ('e@email', '22');
+insert into Faves values ('e@email', '25');
