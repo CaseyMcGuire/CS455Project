cbdc92e25305e6e4b9e44ccbcecb6465e549de6e
Joshua Nance <jnance@pugetsound.edu>
2015-04-26 22:21:39 -0700

    fixes fave query

--- Part3/faves/show.php
+++ Part3/faves/show.php
@@ -39,7 +39,7 @@
             }
             
             // query to select all posts favorited by user with given email
-            $query_string = "select title from faves natural join post where email=\"$email\"";
+            $query_string = "select * from faves natural join post where email=\"$email\"";
             $query = $db->prepare($query_string);
             $query->execute();
             
