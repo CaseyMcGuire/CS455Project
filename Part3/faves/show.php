a3f9c4639cc9e4e9cdb2bf0c34c6e3c1f9b6c6e0
Joshua Nance <jnance@pugetsound.edu>
2015-04-26 17:03:51 -0700

    testing

--- Part3/faves/show.php
+++ Part3/faves/show.php
@@ -51,9 +51,7 @@
                 $query2 = $db->prepare($title_query);
                 $query2->execute();
                 $title_result = $query2->fetchAll();
-                print "-----------------".$title_result;
-                
-                foreach($title_result as $name){print "printing". $name['title'];}
+                print "-----------------".$title_result['title'];
                 
                 echo "<h3><a href=\"/posts/show.php?id=".$tuple['id']."\">".$tuple['id']."</a></h3>";
                 
