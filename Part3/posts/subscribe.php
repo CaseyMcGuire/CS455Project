7b385006bc8416d65b885e7bba8af802387368d3
Joshua Nance <jnance@pugetsound.edu>
2015-04-26 23:46:30 -0700

    adds try catch

--- Part3/posts/subscribe.php
+++ Part3/posts/subscribe.php
@@ -1,15 +1,21 @@
 
 <?php
 
-$db = new PDO("sqlite:../database/blog.sqlite3");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    $db = new PDO("sqlite:../database/blog.sqlite3");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$email = $_GET['subscriber'];
-$id_str = $_GET['id'];
-$id = intval($id_str);
+    $email = $_GET['subscriber'];
+    $id_str = $_GET['id'];
+    $id = intval($id_str);
 
-$db->exec("insert into faves values($email,$id)");
+    $db->exec("insert into faves values($email,$id)");
 
-header("../faves/show.php");
+    header("../faves/show.php");
+}
+catch(PDOException $e) {
+    print "Exception ".$e->getMessage();
+}
+
 
 ?>
